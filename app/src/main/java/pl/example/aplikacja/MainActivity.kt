a15420983f75cdb39d47906c0add904c66214cca
BartoszBedyk <bartoszbedyk@gmail.com>
2025-10-23 23:45:33 +0200

    cicd fix v3 add github token

--- app/src/main/java/pl/example/aplikacja/MainActivity.kt
+++ app/src/main/java/pl/example/aplikacja/MainActivity.kt
@@ -29,9 +29,7 @@ class MainActivity : ComponentActivity() {
             return
         }
 
-        // clearToken(applicationContext)
-        // saveToken(applicationContext, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJteWF1ZGllbmNlIiwiaXNzIjoibXlpc3N1ZXIiLCJ1c2VySWQiOiJmNjk5MzZkYy0wOTYyLTQ4ZDItYTJjMi1hNWRmNDg1NzM5MTciLCJ1c2VybmFtZSI6Im0ubUB3cC5wbCIsInVzZXJUeXBlIjoiUEFUSUVOVCIsImV4cCI6MTc0MzAwMDI5OH0.V3Y8pwigJJW-nNhQ2vSZfiwczRlTRYOGDbOuvT3g6_o")
-        val bluetoothViewModel = ViewModelProvider(this).get(BluetoothViewModel::class.java)
+            val bluetoothViewModel = ViewModelProvider(this).get(BluetoothViewModel::class.java)
         val token = getToken(applicationContext) ?: savedInstanceState?.getString("token")
 
         val userType = if (token != null) {
@@ -39,7 +37,7 @@ class MainActivity : ComponentActivity() {
             val userTypeString = decoded.getClaim("userType").asString()
             try {
                 UserType.valueOf(userTypeString ?: UserType.PATIENT.name)
-            } catch (e: IllegalArgumentException) {
+            } catch (_: IllegalArgumentException) {
                 UserType.PATIENT
             }
         } else {
